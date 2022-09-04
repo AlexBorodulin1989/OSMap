261e67771a44bbe9b2f811ffa1e2195150aeb9b8
Alexandr <aleksanderborodoolin@mail.ru>
2022-09-04 11:32:35 +0300

    Add licenses

--- OSMap/OSMap/Shared/MetalView/MetalView.swift
+++ OSMap/OSMap/Shared/MetalView/MetalView.swift
@@ -1,9 +1,40 @@
-//
-//  MetalView.swift
-//  Pipeline
-//
-//  Created by Aleksandr Borodulin on 03.09.2022.
-//
+///
+/// MetalView.swift
+/// Pipeline
+///
+/// Created by Aleksandr Borodulin on 03.09.2022.
+///
+/// Copyright (c) 2022 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import SwiftUI
 import MetalKit
@@ -16,7 +47,7 @@ typealias ViewRepresentable = UIViewRepresentable
 typealias UniversalView = UIView
 #endif
 
-struct MetalView: ViewRepresentable {
+struct OSMapView: ViewRepresentable {
     @State private var metalView = MTKView()
 
 #if os(macOS)
@@ -43,11 +74,11 @@ struct MetalView: ViewRepresentable {
     }
 
     class Coordinator: NSObject {
-        let parent: MetalView
+        let parent: OSMapView
 
         private var renderer: Renderer!
 
-        init(_ parent: MetalView) {
+        init(_ parent: OSMapView) {
             self.parent = parent
 
             super.init()
@@ -60,7 +91,7 @@ struct MetalView: ViewRepresentable {
 struct MetalView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            MetalView()
+            OSMapView()
         }
     }
 }
